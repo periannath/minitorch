682a492938a6b9d8ccb326d9d636e2f66e39ef39
Peri <yu65789@gmail.com>
2026-01-06 13:34:20 +0900

    [Task 1.1] Implement numerical derivative
    This commit implements numerical derivative

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Any, Iterable, List, Tuple
+from typing import Any, Iterable, Tuple
 
 from typing_extensions import Protocol
 
@@ -22,8 +22,14 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError('Need to implement for Task 1.1')
+    x_2 = list(vals)
+    x_2[arg] += epsilon
+
+    x_1 = list(vals)
+    x_1[arg] -= epsilon
+
+    ret = (f(*x_2) - f(*x_1)) / (2 * epsilon)
+    return ret
 
 
 variable_count = 1
@@ -62,7 +68,7 @@ def topological_sort(variable: Variable) -> Iterable[Variable]:
         Non-constant Variables in topological order starting from the right.
     """
     # TODO: Implement for Task 1.4.
-    raise NotImplementedError('Need to implement for Task 1.4')
+    raise NotImplementedError("Need to implement for Task 1.4")
 
 
 def backpropagate(variable: Variable, deriv: Any) -> None:
@@ -77,7 +83,7 @@ def backpropagate(variable: Variable, deriv: Any) -> None:
     No return. Should write to its results to the derivative values of each leaf through `accumulate_derivative`.
     """
     # TODO: Implement for Task 1.4.
-    raise NotImplementedError('Need to implement for Task 1.4')
+    raise NotImplementedError("Need to implement for Task 1.4")
 
 
 @dataclass
